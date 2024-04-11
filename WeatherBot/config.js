b675151d3d8dea5f692cedf8db16e6c334f387c3
Surya-0804 <159000250+Surya-0804@users.noreply.github.com>
2024-04-11 09:55:23 +0530

    Update config.js

--- WeatherBot/config.js
+++ WeatherBot/config.js
@@ -1,4 +1,4 @@
 module.exports={
-    token : '6063954474:AAHnWw3fg32CByqeIc-i94qMDfGXhkzU3ZE',
-    weatherApiKey: 'b9ef82503e408077ed5b1232ee7ea91a'
+    token : 'Your_telegram_token',
+    weatherApiKey: 'Your_API_Key'
 };
